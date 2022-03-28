322953e42c133db33a5e914e6328a7aadeefef40
denis kuzmin <znake@ozon.ru>
2022-03-28 20:48:50 +0300

    bld fx

--- src/Eventso.Subscription.Kafka.DeadLetter.Postgres/PoisonEventStoreRegistration.cs
+++ src/Eventso.Subscription.Kafka.DeadLetter.Postgres/PoisonEventStoreRegistration.cs
@@ -8,14 +8,14 @@ namespace Eventso.Subscription.Kafka.DeadLetter.Postgres
     public static class PoisonEventStoreRegistration
     {
         // TODO uncomment later
-        public static IDeadLetterQueueOptions UsePostgresStore(
-            this IDeadLetterQueueOptions options,
-            IConnectionFactory connectionFactory)
-        {
-            options.KafkaListener.Services.Replace(
-                ServiceDescriptor.Singleton<IPoisonEventStore>(new PoisonEventStore(connectionFactory)));
-
-            return options;
-        }
+        // public static IDeadLetterQueueOptions UsePostgresStore(
+        //     this IDeadLetterQueueOptions options,
+        //     IConnectionFactory connectionFactory)
+        // {
+        //     options.KafkaListener.Services.Replace(
+        //         ServiceDescriptor.Singleton<IPoisonEventStore>(new PoisonEventStore(connectionFactory)));
+        //
+        //     return options;
+        // }
     }
 }
